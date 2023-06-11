14072271e9bbc2f795bb289f2ae11a3a9056b7df
FastheDeveloper <seriki2farou@gmail.com>
2023-06-11 20:47:32 +0100

    tweet crud

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -1,36 +1,74 @@
 import {Router} from 'express';
-
+import { PrismaClient } from '@prisma/client';
 const router=Router();
-
+const Prisma=new PrismaClient();
 //-----Tweet CRUD-----
 
 //**Create tweet**
-router.post('/',(req,res)=>{
-    res.status(501).json({error:'not implemented tweet'})
+router.post('/',async (req,res)=>{
+    const {image,userId,content,impressions,}=req.body;
+    try{
+        const result=await Prisma.tweet.create({
+            data:{
+               userId,
+               content,
+               image,
+               impressions,
+               
+                  
+            }
+        })
+        res.json(result)
+    }catch(e){
+        res.status(400).json({error:"user already exists"})
+    }
 }); //create
 
 //**list tweet**
-router.get('/',(req,res)=>{
-    res.status(501).json({error:'not implemented'})
+router.get('/',async (req,res)=>{
+    const allTweet=await Prisma.tweet.findMany(); //prisama .user is table name .findmany is query
+    if(!allTweet) res.status(400).json({error:"tweets not found"} )
+    res.json(allTweet)
 }); //read
 
 //**get one tweet**
-router.get('/:id',(req,res)=>{
+router.get('/:id',async (req,res)=>{
     const {id}=req.params;
-    res.status(501).json({error:`not implemented ${id}`})
+    const tweet=await Prisma.tweet.findUnique({ 
+        where:{id:Number(id)}
+    })
+    if(!tweet) res.status(400).json({error:"tweet not found"} ) 
+    res.json(tweet)
 }); //read
 
 //**update tweet**
-router.put('/:id',(req,res)=>{
+router.put('/:id',async (req,res)=>{
     const {id}=req.params;
-    res.status(501).json({error:`not implemented tweet ${id}`})
+    const {impressions}=req.body;
+
+    try{
+        const result= await Prisma.tweet.update({
+            where:{id:Number(id)},
+            data:{
+                impressions,
+                                
+            }
+        })
+        res.json(result)
+    }catch(e){
+        res.status(400).json({error:"user not found"})
+    }
 }); //update
 
 
 //**delete tweet**
-router.delete('/:id',(req,res)=>{
+router.delete('/:id',async (req,res)=>{
     const {id}=req.params;
-    res.status(501).json({error:`not implemented ${id}`})
+    await Prisma.tweet.delete({
+        where:{id:Number(id)}
+    })
+    
+    res.sendStatus(200)
 })
 
 
